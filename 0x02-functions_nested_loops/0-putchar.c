d842915f9f98f42f7d449cd2559b67ca884c7eae
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2023-03-22 02:35:34 +0000

    small change

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+int main(void)
+{
+	char w[] = "_putchar";
+	int i = 0;
+	while(w[i])
+	{
+		_putchar(w[i]);
+	}
+	_putchar('\n');
+}
+
 void _putchar(char c)
 {
     putchar(c);
