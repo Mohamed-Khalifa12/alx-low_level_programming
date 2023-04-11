cb609d4eed4fa3372e2c5b0b7af9aa6138cf1907
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2023-04-11 20:48:58 +0200

    0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,13 +10,14 @@
  * @argv: argument vector
  * Return: 0 on success, 1 on failure
  */
-int checker(int argc, int i, unsigned int j, char *argv[])
+int checker(int argc, char *argv[])
 {
-	for (i = 1; i <= argc; i++)
-		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-			if (isdigit(argv[i][j]) == 0)
-				return (1);
-	return (0);
+	int i , j; 
+	for (i = 1; i < argc; i++)
+		for (j = 0; *argv[i] != '\0' && j < strlen(argv[i]); j++)
+			if (!isdigit(argv[i][j]))
+				return 1;
+	return 0;
 }
 /**
  * main - adds all arguments together if they are digits.
@@ -29,7 +30,7 @@ int main(int argc, char *argv[])
 	int result, i;
 
 	result = 0;
-	if (checker(argc, 1, 0, argv) == 1)
+	if (checker(argc, argv))
 	{
 		printf("Error\n");
 		return (1);
