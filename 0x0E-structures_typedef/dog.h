07d01b942a103e60acbd4969db893cc53761f5af
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2023-04-17 13:31:37 +0200

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct my_struct - Short description
+ * @a: First member
+ * @b: Second member
+ * @c: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+    char* name;
+    float agr;
+    char* owner;
+};
