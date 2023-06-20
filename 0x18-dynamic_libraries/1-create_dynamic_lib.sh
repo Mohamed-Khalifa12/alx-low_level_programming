e9734affdca4a5d865df8b6978131784092a73d0
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2023-06-20 05:43:26 +0300

    Task #2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-gcc -shared -o liball.so *.c
+
+# Get all .c files in the current directory
+c_files=$(ls *.c)
+
+# Generate the compilation command
+compile_cmd="gcc -shared -o liball.so $c_files"
+
+# Execute the compilation command
+eval $compile_cmd
